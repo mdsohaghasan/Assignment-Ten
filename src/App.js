1a49df843f379fb2d69d724dbd8aa59685c8a2c0
Sohag Hasan <mailsohaghasan@gmail.com>
2022-04-17 15:24:20 +0600

    route update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Headers from './Components/Headers/Headers';
 import Home from './Components/Home/Home';
 import Register from './Components/Register/Register';
 import Signin from './Components/Signin/Signin';
+import Services from './Components/Services/Services';
+import Blogs from './Components/Blogs/Blogs';
+import About from './Components/About/About';
 
 
 function App() {
@@ -14,6 +17,10 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/Register" element={<Register></Register>}></Route>
         <Route path="/Signin" element={<Signin></Signin>}></Route>
+        <Route path="/Services" element={<Services></Services>}></Route>
+        <Route path="/Blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/About" element={<About></About>}></Route>
+
 
       </Routes>
     </div>
