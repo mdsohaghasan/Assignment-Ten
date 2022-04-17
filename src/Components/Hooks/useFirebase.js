728391056db7ff4835afd25fb5de0b6113b46631
Sohag Hasan <mailsohaghasan@gmail.com>
2022-04-17 18:19:02 +0600

    problem

--- src/Components/Hooks/useFirebase.js
+++ src/Components/Hooks/useFirebase.js
@@ -1,10 +1,24 @@
 import React, { useState } from 'react'
+import app from '../firebase.init';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+
+const auth = getAuth(app);
+const googleprovider = new GoogleAuthProvider();
 
 const useFirebase = () => {
     const [user, setuser] = useState({});
 
     const signInWithGoogle = () => {
-        console.log('sign in soon')
+        signInWithPopup(auth, googleprovider)
+            .than(result => {
+                const user = result.user
+                setuser(user)
+                console.log(user);
+            }
+
+            )
+        // console.log('sign in soon')
     }
     return { user, signInWithGoogle }
 
--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -4,10 +4,11 @@ import './Signin.css';
 
 function Signin() {
   const { signInWithGoogle } = useFirebase();
+
   return (
     <div>
       <h2>SignIn Now</h2>
-      <button onClick={signInWithGoogle} className="googleBtn">Google Singin</button>
+      <button onClick={signInWithGoogle} >Google Singin</button>
       <div>
         <input type="email" placeholder='your email' /> <br />
         <input type="password" placeholder='your password' /> <br />
