7e71292e159d4b2233388ffd7de24c7091472667
Sohag Hasan <mailsohaghasan@gmail.com>
2022-04-18 07:04:34 +0600

    google btn fix 2

--- src/Components/Hooks/useFirebase.js
+++ src/Components/Hooks/useFirebase.js
@@ -1,22 +1,22 @@
 import React, { useState } from 'react'
-// import app from '../firebase.init';
-// import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from '../../firebase.init';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 
-// const auth = getAuth(app);
-// const googleprovider = new GoogleAuthProvider();
+const auth = getAuth(app);
+const googleprovider = new GoogleAuthProvider();
 
 const useFirebase = () => {
     const [user, setuser] = useState({});
 
     const signInWithGoogle = () => {
-        // signInWithPopup(auth, googleprovider)
-        //     .than(result => {
-        //         const user = result.user
-        //         setuser(user)
-        //         console.log(user);
-        //     }
-        //     )
+        signInWithPopup(auth, googleprovider)
+            .than(result => {
+                const user = result.user
+                setuser(user)
+                console.log(user);
+            }
+            )
         console.log('sign in soon')
     }
     return { user, signInWithGoogle }
