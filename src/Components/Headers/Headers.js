4960132ad8c131358d720a765aaabc1d20701670
Sohag Hasan <mailsohaghasan@gmail.com>
2022-04-18 09:31:03 +0600

    firebase hook

--- src/Components/Headers/Headers.js
+++ src/Components/Headers/Headers.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import './Headers.css';
 import { Link } from 'react-router-dom'
-
+import useFirebase from '../Hooks/useFirebase';
 
 function Headers() {
+  const { user } = useFirebase();
   return (
     <div className='Headers'>
       <Link to="/">Home</Link>
-      <Link to="/Signin">Signin</Link>
+      {user.uid ? <button></button> :
+        < Link to="/Signin">Signin</Link>}
       <Link to="/Register">Register</Link>
       <Link to="/Services">Service</Link>
       <Link to="/Blogs">Blog</Link>
       <Link to="/About">About</Link>
-    </div>
+    </div >
   )
 }
 
