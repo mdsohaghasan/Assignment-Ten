da5ac027792310d1685eea749f09c64c960819b2
Sohag Hasan <mailsohaghasan@gmail.com>
2022-04-19 19:57:38 +0600

    service

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,18 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      <div>
-        <div className=''>About
-          <h1>Hi, I Am Sohag Hasan</h1>
-          <h3>FrontEnd Developer</h3>
-          <button>Join Us</button>
-        </div>
-        <div className=''>
-          <img src='' alt='' />
-        </div>
-      </div>
-    </div>
+    <div>About</div>
   )
 }
 
