ad34e521b9335ef43b06ae6d515748735c29f658
Sohag Hasan <mailsohaghasan@gmail.com>
2022-04-19 18:13:24 +0600

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,10 +3,15 @@ import React from 'react'
 function About() {
   return (
     <div>
-      <div>About</div>
-      <div>About Us</div>
       <div>
-
+        <div className=''>About
+          <h1>Hi, I Am Sohag Hasan</h1>
+          <h3>FrontEnd Developer</h3>
+          <button>Join Us</button>
+        </div>
+        <div className=''>
+          <img src='' alt='' />
+        </div>
       </div>
     </div>
   )
